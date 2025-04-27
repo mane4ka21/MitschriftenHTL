8fcaefa8f5d51c0790d0c4b6576f83aec4e9a6c1
Mariia <mimi.03.21pr@gmail.com>
2025-04-28 00:15:03 +0200

    2 zaliv app

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ def home():
     return render_template("index.html", courses=courses)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
